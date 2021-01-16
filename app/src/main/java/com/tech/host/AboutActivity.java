e1ceb47ded6aa9159fe70f0de5e8b06a977c1b98
Ryanika-Coder <niicoremusic@gmail.com>
2021-01-16 16:20:41 +0800

    Update 1.1

---
+++ app/src/main/java/com/tech/host/AboutActivity.java
@@ -0,0 +1,242 @@
+package com.tech.host;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.*;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import com.google.android.material.appbar.AppBarLayout;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.webkit.*;
+import android.animation.*;
+import android.view.animation.*;
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import org.json.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import android.widget.LinearLayout;
+import de.hdodenhof.circleimageview.*;
+import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.DialogFragment;
+
+
+public class AboutActivity extends  AppCompatActivity  { 
+	
+	
+	private Toolbar _toolbar;
+	private AppBarLayout _app_bar;
+	private CoordinatorLayout _coordinator;
+	
+	private ArrayList<HashMap<String, Object>> list = new ArrayList<>();
+	
+	private LinearLayout linear1;
+	private LinearLayout linear8;
+	private LinearLayout linear9;
+	private LinearLayout linear10;
+	private CircleImageView circleimageview1;
+	private TextView textview3;
+	private TextView textview4;
+	private ListView listview2;
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.about);
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		
+		_app_bar = (AppBarLayout) findViewById(R.id._app_bar);
+		_coordinator = (CoordinatorLayout) findViewById(R.id._coordinator);
+		_toolbar = (Toolbar) findViewById(R.id._toolbar);
+		setSupportActionBar(_toolbar);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setHomeButtonEnabled(true);
+		_toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _v) {
+				onBackPressed();
+			}
+		});
+		linear1 = (LinearLayout) findViewById(R.id.linear1);
+		linear8 = (LinearLayout) findViewById(R.id.linear8);
+		linear9 = (LinearLayout) findViewById(R.id.linear9);
+		linear10 = (LinearLayout) findViewById(R.id.linear10);
+		circleimageview1 = (CircleImageView) findViewById(R.id.circleimageview1);
+		textview3 = (TextView) findViewById(R.id.textview3);
+		textview4 = (TextView) findViewById(R.id.textview4);
+		listview2 = (ListView) findViewById(R.id.listview2);
+	}
+	
+	private void initializeLogic() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+			getWindow().setNavigationBarColor(Color.parseColor("#008dcd"));
+		}
+		circleimageview1.setImageResource(R.drawable.app_icon);
+		listview2.setDivider(new ColorDrawable(0xFFFFFFFF));
+		listview2.setDividerHeight(1);
+		{
+			HashMap<String, Object> _item = new HashMap<>();
+			_item.put("list", "Created by Ryanika");
+			list.add(_item);
+		}
+		
+		{
+			HashMap<String, Object> _item = new HashMap<>();
+			_item.put("list", "App icon by Techout");
+			list.add(_item);
+		}
+		
+		{
+			HashMap<String, Object> _item = new HashMap<>();
+			_item.put("list", "No Internet icon by Jardson Almeida");
+			list.add(_item);
+		}
+		
+		{
+			HashMap<String, Object> _item = new HashMap<>();
+			_item.put("list", "Made for TechHost");
+			list.add(_item);
+		}
+		
+		{
+			HashMap<String, Object> _item = new HashMap<>();
+			_item.put("list", "Tester :\n- Techout\n- JohnlsJoker");
+			list.add(_item);
+		}
+		
+		listview2.setAdapter(new Listview2Adapter(list));
+		((BaseAdapter)listview2.getAdapter()).notifyDataSetChanged();
+	}
+	
+	@Override
+	protected void onActivityResult(int _requestCode, int _resultCode, Intent _data) {
+		
+		super.onActivityResult(_requestCode, _resultCode, _data);
+		
+		switch (_requestCode) {
+			
+			default:
+			break;
+		}
+	}
+	
+	@Override
+	public void onBackPressed() {
+		finish();
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		finish();
+	}
+	public class Listview2Adapter extends BaseAdapter {
+		ArrayList<HashMap<String, Object>> _data;
+		public Listview2Adapter(ArrayList<HashMap<String, Object>> _arr) {
+			_data = _arr;
+		}
+		
+		@Override
+		public int getCount() {
+			return _data.size();
+		}
+		
+		@Override
+		public HashMap<String, Object> getItem(int _index) {
+			return _data.get(_index);
+		}
+		
+		@Override
+		public long getItemId(int _index) {
+			return _index;
+		}
+		@Override
+		public View getView(final int _position, View _v, ViewGroup _container) {
+			LayoutInflater _inflater = (LayoutInflater)getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			View _view = _v;
+			if (_view == null) {
+				_view = _inflater.inflate(R.layout.list, null);
+			}
+			
+			final LinearLayout linear1 = (LinearLayout) _view.findViewById(R.id.linear1);
+			final TextView textview1 = (TextView) _view.findViewById(R.id.textview1);
+			
+			textview1.setText(list.get((int)_position).get("list").toString());
+			
+			return _view;
+		}
+	}
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input){
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels(){
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels(){
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+	
+}
